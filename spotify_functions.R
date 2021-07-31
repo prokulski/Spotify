79680220be6c9948b33ba897d6800d2fb865d35c
Łukasz Prokulski <prokulski@gmail.com>
2021-07-31 11:34:43 +0200

    reformat z poziomu vscode

--- spotify_functions.R
+++ spotify_functions.R
@@ -8,11 +8,12 @@ library(RCurl)
 # funkcja zwraca token
 getSpotifyToken <- function() {
    res <- POST("https://accounts.spotify.com/api/token",
-               body = list(grant_type = "client_credentials"),
-               config = add_headers("Authorization" = paste0("Basic ", base64(paste(client_id,client_secret,sep=':'))[[1]])),
-               encode = "form")
+      body = list(grant_type = "client_credentials"),
+      config = add_headers("Authorization" = paste0("Basic ", base64(paste(client_id, client_secret, sep = ":"))[[1]])),
+      encode = "form"
+   )
 
-   if(res$status_code == 200) {
+   if (res$status_code == 200) {
       return(content(res)$access_token)
    } else {
       return(NA)
@@ -23,37 +24,48 @@ getSpotifyToken <- function() {
 
 # funkcja pobiera listę utworów z biblioteki użytkownika
 getMySporifyLibrary <- function() {
-
    library_tracks <- tibble()
 
    is_next <- TRUE
 
    query_url <- "https://api.spotify.com/v1/me/tracks?limit=50"
 
-   while(is_next) {
-      response <- GET(query_url,
-                      add_headers("Authorization:" = paste("Bearer", spotify_token),
-                                  "Content-Type:" = "application/json",
-                                  "Accept:" = "application/json"))
+   while (is_next) {
+      response <- GET(
+         query_url,
+         add_headers(
+            "Authorization:" = paste("Bearer", spotify_token),
+            "Content-Type:" = "application/json",
+            "Accept:" = "application/json"
+         )
+      )
 
-      if(response$status_code == 200) {
-
+      if (response$status_code == 200) {
          response_json <- rawToChar(response$content) %>% iconv(from = "UTF-8", to = Sys.getenv("CHARSET"))
          query_results <- fromJSON(response_json, flatten = TRUE)
 
          library_tracks_tmp <- query_results$items %>%
             select(added_at,
-                   track_id = track.id,
-                   track = track.name,
-                   popularity = track.popularity,
-                   duration_s = track.duration_ms,
-                   explicit = track.explicit,
-                   album = track.album.name) %>%
-            mutate(duration_s = duration_s/1000)
+               track_id = track.id,
+               track = track.name,
+               popularity = track.popularity,
+               duration_s = track.duration_ms,
+               explicit = track.explicit,
+               album = track.album.name
+            ) %>%
+            mutate(duration_s = duration_s / 1000)
 
 
-         library_tracks_tmp$artist <- map_chr(.f = function(x) x$name %>% unlist %>% str_c(collapse = ", "), query_results$items$track.artists)
-         library_tracks_tmp$artist_id <- map_chr(.f = function(x) x$id %>% unlist %>% str_c(collapse = ", "), query_results$items$track.artists)
+         library_tracks_tmp$artist <- map_chr(.f = function(x) {
+            x$name %>%
+               unlist() %>%
+               str_c(collapse = ", ")
+         }, query_results$items$track.artists)
+         library_tracks_tmp$artist_id <- map_chr(.f = function(x) {
+            x$id %>%
+               unlist() %>%
+               str_c(collapse = ", ")
+         }, query_results$items$track.artists)
 
          library_tracks <- bind_rows(library_tracks, library_tracks_tmp)
 
@@ -71,14 +83,16 @@ getMySporifyLibrary <- function() {
 
 # Funkcja pobiera cechy audio utworu track_id
 getSporifyTrackFeatures <- function(track_id) {
+   response <- GET(
+      paste0("https://api.spotify.com/v1/audio-features/", track_id),
+      add_headers(
+         "Authorization:" = paste("Bearer", spotify_token),
+         "Content-Type:" = "application/json",
+         "Accept:" = "application/json"
+      )
+   )
 
-   response <- GET(paste0("https://api.spotify.com/v1/audio-features/", track_id),
-                   add_headers("Authorization:" = paste("Bearer", spotify_token),
-                               "Content-Type:" = "application/json",
-                               "Accept:" = "application/json"))
-
-   if(response$status_code == 200) {
-
+   if (response$status_code == 200) {
       response_json <- rawToChar(response$content) %>% iconv(from = "UTF-8", to = Sys.getenv("CHARSET"))
       query_results <- fromJSON(response_json, flatten = TRUE)
 
@@ -94,7 +108,8 @@ getSporifyTrackFeatures <- function(track_id) {
          liveness = query_results$liveness,
          valence = query_results$valence,
          tempo = query_results$tempo,
-         time_signature = query_results$time_signature)
+         time_signature = query_results$time_signature
+      )
    } else {
       return(tibble())
    }
@@ -106,55 +121,76 @@ getSporifyTrackFeatures <- function(track_id) {
 
 # Funkcja pobiera playlistę playlist_id użytkownika user_id
 getSpotifyUserPlaylist <- function(user_id, playlist_id) {
-
    playlist_tracks <- tibble()
 
    is_next <- TRUE
 
    query_url <- paste0("https://api.spotify.com/v1/users/", user_id, "/playlists/", playlist_id, "?limit=100")
 
-   while(is_next) {
-      response <- GET(query_url,
-                      add_headers("Authorization:" = paste("Bearer", spotify_token),
-                                  "Content-Type:" = "application/json",
-                                  "Accept:" = "application/json"))
+   while (is_next) {
+      response <- GET(
+         query_url,
+         add_headers(
+            "Authorization:" = paste("Bearer", spotify_token),
+            "Content-Type:" = "application/json",
+            "Accept:" = "application/json"
+         )
+      )
 
-      if(response$status_code == 200) {
-
+      if (response$status_code == 200) {
          response_json <- rawToChar(response$content)
          query_results <- fromJSON(response_json, flatten = TRUE)
 
-         if(!is.null(query_results$tracks)) {
-            playlist_tracks_tmp <- query_results$tracks$items %>% select(added_at,
-                                                                         track_id = track.id,
-                                                                         track = track.name,
-                                                                         popularity = track.popularity,
-                                                                         duration_s = track.duration_ms,
-                                                                         explicit = track.explicit,
-                                                                         album_id = track.album.id,
-                                                                         album = track.album.name) %>%
-               mutate(duration_s = duration_s/1000)
+         if (!is.null(query_results$tracks)) {
+            playlist_tracks_tmp <- query_results$tracks$items %>%
+               select(added_at,
+                  track_id = track.id,
+                  track = track.name,
+                  popularity = track.popularity,
+                  duration_s = track.duration_ms,
+                  explicit = track.explicit,
+                  album_id = track.album.id,
+                  album = track.album.name
+               ) %>%
+               mutate(duration_s = duration_s / 1000)
 
 
-            playlist_tracks_tmp$artist <- map_chr(.f = function(x) x$name %>% unlist %>% str_c(collapse = ", "), query_results$tracks$items$track.artists)
-            playlist_tracks_tmp$artist_id <- map_chr(.f = function(x) x$id %>% unlist %>% str_c(collapse = ", "), query_results$tracks$items$track.artists)
+            playlist_tracks_tmp$artist <- map_chr(.f = function(x) {
+               x$name %>%
+                  unlist() %>%
+                  str_c(collapse = ", ")
+            }, query_results$tracks$items$track.artists)
+            playlist_tracks_tmp$artist_id <- map_chr(.f = function(x) {
+               x$id %>%
+                  unlist() %>%
+                  str_c(collapse = ", ")
+            }, query_results$tracks$items$track.artists)
 
             query_url <- query_results$tracks$`next`
-
          } else {
-            playlist_tracks_tmp <- query_results$items %>% select(added_at,
-                                                                  track_id = track.id,
-                                                                  track = track.name,
-                                                                  popularity = track.popularity,
-                                                                  duration_s = track.duration_ms,
-                                                                  explicit = track.explicit,
-                                                                  album_id = track.album.id,
-                                                                  album = track.album.name) %>%
-               mutate(duration_s = duration_s/1000)
+            playlist_tracks_tmp <- query_results$items %>%
+               select(added_at,
+                  track_id = track.id,
+                  track = track.name,
+                  popularity = track.popularity,
+                  duration_s = track.duration_ms,
+                  explicit = track.explicit,
+                  album_id = track.album.id,
+                  album = track.album.name
+               ) %>%
+               mutate(duration_s = duration_s / 1000)
 
 
-            playlist_tracks_tmp$artist <- map_chr(.f = function(x) x$name %>% unlist %>% str_c(collapse = ", "), query_results$items$track.artists)
-            playlist_tracks_tmp$artist_id <- map_chr(.f = function(x) x$id %>% unlist %>% str_c(collapse = ", "), query_results$items$track.artists)
+            playlist_tracks_tmp$artist <- map_chr(.f = function(x) {
+               x$name %>%
+                  unlist() %>%
+                  str_c(collapse = ", ")
+            }, query_results$items$track.artists)
+            playlist_tracks_tmp$artist_id <- map_chr(.f = function(x) {
+               x$id %>%
+                  unlist() %>%
+                  str_c(collapse = ", ")
+            }, query_results$items$track.artists)
 
             query_url <- query_results$`next`
          }
@@ -175,18 +211,20 @@ getSpotifyUserPlaylist <- function(user_id, playlist_id) {
 # Get an Artist
 # https://api.spotify.com/v1/artists/{id}
 getSpotifyArtist <- function(artist_id) {
+   response <- GET(
+      paste0("https://api.spotify.com/v1/artists/", artist_id),
+      add_headers(
+         "Authorization:" = paste("Bearer", spotify_token),
+         "Content-Type:" = "application/json",
+         "Accept:" = "application/json"
+      )
+   )
 
-   response <- GET(paste0("https://api.spotify.com/v1/artists/", artist_id),
-                   add_headers("Authorization:" = paste("Bearer", spotify_token),
-                               "Content-Type:" = "application/json",
-                               "Accept:" = "application/json"))
-
-   if(response$status_code == 200) {
-
+   if (response$status_code == 200) {
       response_json <- rawToChar(response$content)
       query_results <- fromJSON(response_json, flatten = TRUE)
 
-      if(length(query_results$genres) != 0) {
+      if (length(query_results$genres) != 0) {
          df <- tibble(genres = query_results$genres)
       } else {
          df <- tibble(genres = NA)
